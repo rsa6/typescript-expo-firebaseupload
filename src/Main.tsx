c4ffae2d9539e3eadfad9d3d905ede9854dd23c8
rsa6 <rsa6@naver.com>
2019-01-26 22:03:22 +0900

    last

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,18 +1,37 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, TouchableOpacity } from 'react-native';
+import { Camera, Permissions } from 'expo';
 import styles from '../styles';
 
 class Main extends React.Component<object, object> {
-  state = {
+  public state = {
     title: 'hello'
   }
   
-  render() {
+  public render() {
     return (
       <View style={styles.container}>
         <Text style={styles.textH1}>{this.state.title}</Text>
       </View>
     );
+  }
+}
+
+class FileSelect extends React.Component<object, object> {
+  public state = {
+    hasCameraPermission: null,
+    type: Camera.Constants.Type.back,
+  };
+
+  public async componentDidMount() {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    this.setState({ hasCameraPermission: status === 'granted' });
+  }
+
+  public render() {
+    return (
+      <Text>some kind of fileselect</Text>
+    )
   }
 }
 
